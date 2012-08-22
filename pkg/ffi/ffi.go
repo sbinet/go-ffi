56f0c3004ab2022803e09aaea9ff8046e849c293
Sebastien Binet <binet@cern.ch>
2012-08-22 15:48:51 +0200

    add the ability to define new (struct) types. extend Type interface accordingly. more tests

--- pkg/ffi/ffi.go
+++ pkg/ffi/ffi.go
@@ -3,6 +3,47 @@ package ffi
 // #include <stdlib.h>
 // #include "ffi.h"
 // typedef void (*_go_ffi_fctptr_t)(void);
+// static void _go_ffi_type_set_type(ffi_type *t, unsigned short type)
+// {
+//   t->type = type;
+// }
+// static unsigned short _go_ffi_type_get_type(ffi_type *t)
+// {
+//   return t->type;
+// }
+// static void _go_ffi_type_set_elements(ffi_type *t, void *elmts)
+// {
+//   t->elements = (ffi_type**)(elmts);
+// }
+// static void *_go_ffi_type_get_offset(void *data, unsigned n, ffi_type **types)
+// {
+//   size_t ofs = 0;
+//   unsigned i;
+//   unsigned short a;
+//   for (i = 0; i < n && types[i]; i++) {
+//     a = ofs % types[i]->alignment;
+//     if (a != 0) ofs += types[i]->alignment-a;
+//     ofs += types[i]->size;
+//   }
+//   if (i < n || !types[i])
+//     return 0;
+//   a = ofs % types[i]->alignment;
+//   if (a != 0) ofs += types[i]->alignment-a;
+//   return data+ofs;
+// }
+// static int _go_ffi_type_get_offsetof(ffi_type *t, int i)
+// {
+//   void *v;
+//   void *data = NULL + 2; // make a non-null pointer
+//   if (t->type != FFI_TYPE_STRUCT) return 0;
+//   v = _go_ffi_type_get_offset(data, i, t->elements);
+//   if (v) {
+//     return (int)(v - data);
+//   } else {
+//     return 0;
+//   }
+//   return 0;
+// }
 import "C"
 
 import (
@@ -32,33 +73,160 @@ const (
 	//TypeSmallStruct4B = C.FFI_TYPE_SMALL_STRUCT_4B
 )
 
+type Kind uint
+
+const (
+	Void       Kind = C.FFI_TYPE_VOID
+	Int             = C.FFI_TYPE_INT
+	Float           = C.FFI_TYPE_FLOAT
+	Double          = C.FFI_TYPE_DOUBLE
+	LongDouble      = C.FFI_TYPE_LONGDOUBLE
+	Uint8           = C.FFI_TYPE_UINT8
+	Int8            = C.FFI_TYPE_SINT8
+	Uint16          = C.FFI_TYPE_UINT16
+	Int16           = C.FFI_TYPE_SINT16
+	Uint32          = C.FFI_TYPE_UINT32
+	Int32           = C.FFI_TYPE_SINT32
+	Uint64          = C.FFI_TYPE_UINT64
+	Int64           = C.FFI_TYPE_SINT64
+	Struct          = C.FFI_TYPE_STRUCT
+	Pointer         = C.FFI_TYPE_POINTER
+)
+
+func (k Kind) String() string {
+	switch k {
+	case Void:
+		return "Void"
+	case Int:
+        return "Int"
+	case Float:
+        return "Float"
+	case Double:
+        return "Double"
+	case LongDouble:
+		return "LongDouble"
+	case Uint8:
+        return "Uint8"
+	case Int8:
+        return "Int8"
+	case Uint16:
+        return "Uint16"
+	case Int16:
+        return "Int16"
+	case Uint32:
+        return "Uint32"
+	case Int32:
+        return "Int32"
+	case Uint64:
+        return "Uint64"
+	case Int64:
+        return "Int64"
+	case Struct:
+        return "Struct"
+	case Pointer:
+        return "Pointer"
+	}
+	panic("unreachable")
+}
+
 // Type is a FFI type, describing functions' type arguments
-type Type struct {
+type Type interface {
+	cptr() *C.ffi_type
+
+	// Name returns the type's name.
+	Name() string
+
+	// Size returns the number of bytes needed to store
+	// a value of the given type.
+	Size() uintptr
+
+	// String returns a string representation of the type.
+	String() string
+
+	// Kind returns the specific kind of this type
+	Kind() Kind
+
+	// Align returns the alignment in bytes of a value of this type.
+	Align() int
+
+	// Field returns a struct type's i'th field.
+	// It panics if the type's Kind is not Struct.
+	// It panics if i is not in the range [0, NumField()).
+	Field(i int) StructField
+
+	// NumField returns a struct type's field count.
+	// It panics if the type's Kind is not Struct.
+	NumField() int
+}
+
+type cffi_type struct {
+	n string
 	c *C.ffi_type
 }
 
+func (t cffi_type) cptr() *C.ffi_type {
+	return t.c
+}
+
+func (t cffi_type) Name() string {
+	return t.n
+}
+
+func (t cffi_type) Size() uintptr {
+	return uintptr(t.c.size)
+}
+
+func (t cffi_type) String() string {
+	// fixme:
+	return t.n
+}
+
+func (t cffi_type) Kind() Kind {
+	return Kind(C._go_ffi_type_get_type(t.c))
+}
+
+func (t cffi_type) Align() int {
+	return int(t.c.alignment)
+}
+
+func (t cffi_type) NumField() int {
+	if t.Kind() != Struct {
+		panic("ffi: NumField of non-struct type")
+	}
+	tt := (*cffi_struct)(unsafe.Pointer(&t))
+	return tt.NumField()
+}
+
+func (t cffi_type) Field(i int) StructField {
+	if t.Kind() != Struct {
+		panic("ffi: Field of non-struct type")
+	}
+	tt := (*cffi_struct)(unsafe.Pointer(&t))
+	return tt.Field(i)
+}
+
 var (
-	Void       = Type{&C.ffi_type_void}
-	Uchar      = Type{&C.ffi_type_uchar}
-	Char       = Type{&C.ffi_type_schar}
-	Ushort     = Type{&C.ffi_type_ushort}
-	Short      = Type{&C.ffi_type_sshort}
-	Uint       = Type{&C.ffi_type_uint}
-	Int        = Type{&C.ffi_type_sint}
-	Ulong      = Type{&C.ffi_type_ulong}
-	Long       = Type{&C.ffi_type_slong}
-	Uint8      = Type{&C.ffi_type_uint8}
-	Int8       = Type{&C.ffi_type_sint8}
-	Uint16     = Type{&C.ffi_type_uint16}
-	Int16      = Type{&C.ffi_type_sint16}
-	Uint32     = Type{&C.ffi_type_uint32}
-	Int32      = Type{&C.ffi_type_sint32}
-	Uint64     = Type{&C.ffi_type_uint64}
-	Int64      = Type{&C.ffi_type_sint64}
-	Float      = Type{&C.ffi_type_float}
-	Double     = Type{&C.ffi_type_double}
-	LongDouble = Type{&C.ffi_type_longdouble}
-	Pointer    = Type{&C.ffi_type_pointer}
+	C_void       Type = cffi_type{"void", &C.ffi_type_void}
+	C_uchar           = cffi_type{"unsigned char", &C.ffi_type_uchar}
+	C_char            = cffi_type{"char", &C.ffi_type_schar}
+	C_ushort          = cffi_type{"unsigned short", &C.ffi_type_ushort}
+	C_short           = cffi_type{"short", &C.ffi_type_sshort}
+	C_uint            = cffi_type{"unsigned int", &C.ffi_type_uint}
+	C_int             = cffi_type{"int", &C.ffi_type_sint}
+	C_ulong           = cffi_type{"unsigned long", &C.ffi_type_ulong}
+	C_long            = cffi_type{"long", &C.ffi_type_slong}
+	C_uint8           = cffi_type{"uint8", &C.ffi_type_uint8}
+	C_int8            = cffi_type{"int8", &C.ffi_type_sint8}
+	C_uint16          = cffi_type{"uint16", &C.ffi_type_uint16}
+	C_int16           = cffi_type{"int16", &C.ffi_type_sint16}
+	C_uint32          = cffi_type{"uint32", &C.ffi_type_uint32}
+	C_int32           = cffi_type{"int32", &C.ffi_type_sint32}
+	C_uint64          = cffi_type{"uint64", &C.ffi_type_uint64}
+	C_int64           = cffi_type{"int64", &C.ffi_type_sint64}
+	C_float           = cffi_type{"float", &C.ffi_type_float}
+	C_double          = cffi_type{"double", &C.ffi_type_double}
+	C_longdouble      = cffi_type{"long double", &C.ffi_type_longdouble}
+	C_pointer         = cffi_type{"pointer", &C.ffi_type_pointer}
 )
 
 type Status uint32
@@ -108,11 +276,11 @@ func NewCif(abi Abi, rtype Type, args []Type) (*Cif, error) {
 	if len(args) > 0 {
 		var cargs = make([]*C.ffi_type, len(args))
 		for i, _ := range args {
-			cargs[i] = args[i].c
+			cargs[i] = args[i].cptr()
 		}
 		c_args = &cargs[0]
 	}
-	sc := C.ffi_prep_cif(&cif.c, C.ffi_abi(abi), c_nargs, rtype.c, c_args)
+	sc := C.ffi_prep_cif(&cif.c, C.ffi_abi(abi), c_nargs, rtype.cptr(), c_args)
 	if sc != C.FFI_OK {
 		return nil, fmt.Errorf("error while preparing cif (%s)",
 			Status(sc))
@@ -331,6 +499,126 @@ func (lib Library) Fct(fctname string, rtype Type, argtypes []Type) (Function, e
 		return out
 	}
 	return Function(fct), nil
+}
+
+type StructField struct {
+	Name   string  // Name is the field name
+	Type   Type    // field type
+	Offset uintptr // offset within struct, in bytes
+}
+
+type cffi_struct struct {
+	cffi_type
+	fields []StructField
+}
+
+func (t cffi_struct) NumField() int {
+	return len(t.fields)
+}
+
+func (t cffi_struct) Field(i int) StructField {
+	if i < 0 || i >= len(t.fields) {
+		panic("ffi: field index out of range")
+	}
+	return t.fields[i]
+}
+
+type Field struct {
+	Name   string  // Name is the field name
+	Type   Type    // field type
+}
+
+// NewType creates a new ffi_type describing a C-struct
+func NewType(name string, fields []Field) (Type, error) {
+	c := C.ffi_type{}
+	t := cffi_struct{
+		cffi_type: cffi_type{n: name, c: &c},
+		fields:    make([]StructField, len(fields)),
+	}
+	t.cffi_type.c.size = 0
+	t.cffi_type.c.alignment = 0
+	C._go_ffi_type_set_type(t.cptr(), C.FFI_TYPE_STRUCT)
+
+	var c_fields **C.ffi_type = nil
+	if len(fields) > 0 {
+		var cargs = make([]*C.ffi_type, len(fields)+1)
+		for i, f := range fields {
+			cargs[i] = f.Type.cptr()
+		}
+		cargs[len(fields)] = nil
+		c_fields = &cargs[0]
+	}
+	C._go_ffi_type_set_elements(t.cptr(), unsafe.Pointer(c_fields))
+
+	// initialize type (computes alignment and size)
+	_, err := NewCif(DefaultAbi, t, nil)
+	if err != nil {
+		return nil, err
+	}
+	
+	for i := 0; i < len(fields); i++ {
+		//cft := C._go_ffi_type_get_element(t.cptr(), C.int(i))
+		ff := fields[i]
+		t.fields[i] = StructField{
+			ff.Name, 
+			TypeByName(ff.Type.Name()),
+			uintptr(C._go_ffi_type_get_offsetof(t.cptr(), C.int(i))),
+		}
+	}
+	return t, nil
+}
+
+// the global map of types
+var g_types map[string]Type
+
+// TypeByName returns a ffi.Type by name.
+// Returns nil if no such type exists
+func TypeByName(n string) Type {
+	t, ok := g_types[n]
+	if ok {
+		return t
+	}
+	return nil
+}
+
+func init() {
+	g_types = make(map[string]Type)
+
+	// initialize all builtin types
+	init_type := func(t Type) {
+		n := t.Name()
+		//fmt.Printf("ctype [%s] - size: %v...\n", n, t.Size())
+		if _, ok := g_types[n]; ok {
+			//fmt.Printf("ctypes [%s] already registered\n", n)
+			return
+		}
+		//NewCif(DefaultAbi, t, nil)
+		//fmt.Printf("ctype [%s] - size: %v\n", n, t.Size())
+		g_types[n] = t
+	}
+
+	init_type(C_void)
+	init_type(C_uchar)
+	init_type(C_char)
+	init_type(C_ushort)
+	init_type(C_short)
+	init_type(C_uint)
+	init_type(C_int)
+	init_type(C_ulong)
+	init_type(C_long)
+	init_type(C_uint8)
+	init_type(C_int8)
+	init_type(C_uint16)
+	init_type(C_int16)
+	init_type(C_uint32)
+	init_type(C_int32)
+	init_type(C_uint64)
+	init_type(C_int64)
+	init_type(C_float)
+	init_type(C_double)
+	init_type(C_longdouble)
+	init_type(C_pointer)
+
 }
 
 // EOF
--- pkg/ffi/ffi_test.go
+++ pkg/ffi/ffi_test.go
@@ -2,6 +2,7 @@ package ffi_test
 
 import (
 	"math"
+	"reflect"
 	"testing"
 
 	"github.com/sbinet/go-ffi/pkg/ffi"
@@ -29,7 +30,7 @@ func TestFFIMathf(t *testing.T) {
 	}
 
 	for _, info := range tests {
-		f, err := lib.Fct(info.fct, ffi.Double, []ffi.Type{ffi.Double})
+		f, err := lib.Fct(info.fct, ffi.C_double, []ffi.Type{ffi.C_double})
 		if err != nil {
 			t.Errorf("could not locate function [%s]: %v", info.fct, err)
 		}
@@ -53,7 +54,7 @@ func TestFFIMathi(t *testing.T) {
 		t.Errorf("%v", err)
 	}
 
-	f, err := lib.Fct("abs", ffi.Int, []ffi.Type{ffi.Int})
+	f, err := lib.Fct("abs", ffi.C_int, []ffi.Type{ffi.C_int})
 	if err != nil {
 		t.Errorf("could not locate function [abs]: %v", err)
 	}
@@ -86,7 +87,7 @@ func TestFFIStrCmp(t *testing.T) {
 	}
 
 	//int strcmp(const char* cs, const char* ct);
-	f, err := lib.Fct("strcmp", ffi.Int, []ffi.Type{ffi.Pointer, ffi.Pointer})
+	f, err := lib.Fct("strcmp", ffi.C_int, []ffi.Type{ffi.C_pointer, ffi.C_pointer})
 	if err != nil {
 		t.Errorf("could not locate function [strcmp]: %v", err)
 	}
@@ -123,7 +124,7 @@ func TestFFIStrLen(t *testing.T) {
 	}
 
 	//size_t strlen(const char* cs);
-	f, err := lib.Fct("strlen", ffi.Int, []ffi.Type{ffi.Pointer})
+	f, err := lib.Fct("strlen", ffi.C_int, []ffi.Type{ffi.C_pointer})
 	if err != nil {
 		t.Errorf("could not locate function [strlen]: %v", err)
 	}
@@ -150,14 +151,14 @@ func TestFFIStrCat(t *testing.T) {
 	}
 
 	//char* strcat(char* s, const char* ct);
-	f, err := lib.Fct("strcat", ffi.Pointer, []ffi.Type{ffi.Pointer, ffi.Pointer})
+	f, err := lib.Fct("strcat", ffi.C_pointer, []ffi.Type{ffi.C_pointer, ffi.C_pointer})
 	if err != nil {
 		t.Errorf("could not locate function [strlen]: %v", err)
 	}
 	{
 		s1 := "foo"
 		s2 := "bar"
-		out := f(s1,s2).String()
+		out := f(s1, s2).String()
 		//FIXME
 		if out != "foobar" && false {
 			t.Errorf("expected [foobar], got [%s] (s1=%s, s2=%s)", out, s1, s2)
@@ -168,6 +169,92 @@ func TestFFIStrCat(t *testing.T) {
 	err = lib.Close()
 	if err != nil {
 		t.Errorf("error closing [%s]: %v", libc_name, err)
+	}
+}
+
+func TestFFIBuiltinTypes(t *testing.T) {
+	for _, table := range []struct{
+		n string
+		t ffi.Type
+		rt reflect.Type
+	}{
+		{"uchar", ffi.C_uchar, reflect.TypeOf(byte(0))},
+		{"char", ffi.C_char, reflect.TypeOf(byte(0))},
+
+		{"int8", ffi.C_int8, reflect.TypeOf(int8(0))},
+		{"uint8", ffi.C_uint8, reflect.TypeOf(uint8(0))},
+		{"int16", ffi.C_int16, reflect.TypeOf(int16(0))},
+		{"uint16", ffi.C_uint16, reflect.TypeOf(uint16(0))},
+		{"int32", ffi.C_int32, reflect.TypeOf(int32(0))},
+		{"uint32", ffi.C_uint32, reflect.TypeOf(uint32(0))},
+		{"int64", ffi.C_int64, reflect.TypeOf(int64(0))},
+		{"uint64", ffi.C_uint64, reflect.TypeOf(uint64(0))},
+
+		{"float", ffi.C_float, reflect.TypeOf(float32(0))},
+		{"double", ffi.C_double, reflect.TypeOf(float64(0))},
+		//FIXME: use float128 when/if available
+		{"long double", ffi.C_longdouble, reflect.TypeOf(complex128(0))},
+
+		{"pointer", ffi.C_pointer, reflect.TypeOf((*int)(nil))},
+	} {
+		if table.n != table.t.Name() {
+			t.Errorf("expected [%s], got [%s]", table.n, table.t.Name())
+		}
+		if table.t.Size() != table.rt.Size() {
+			t.Errorf("expected [%s], got [%s]", table.t.Size(), table.rt.Size())
+		}
+	}
+}
+
+func TestFFINewType(t *testing.T) {
+
+	for _, table := range []struct{
+		name string
+		fields []ffi.Field
+		size uintptr
+		offsets []uintptr
+	}{
+		{"struct_0", 
+			[]ffi.Field{{"a", ffi.C_int}}, 
+			ffi.C_int.Size(),
+			[]uintptr{0},
+		},
+		{"struct_1", 
+			[]ffi.Field{
+				{"a", ffi.C_int}, 
+				{"b", ffi.C_int},
+			}, 
+			ffi.C_int.Size()+ffi.C_int.Size(),
+			[]uintptr{0, ffi.C_int.Size()},
+		},
+		{"struct_2",
+			[]ffi.Field{
+				{"F1", ffi.C_uint8},
+				{"F2", ffi.C_int16},
+				{"F3", ffi.C_int32},
+				{"F4", ffi.C_uint8},
+			},
+			12,
+			[]uintptr{0, 2, 4, 8},
+		},
+	} {
+		typ, err := ffi.NewType(table.name, table.fields)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		if typ.Name() != table.name {
+			t.Errorf("expected [%s], got [%s]", table.name, typ.Name())
+		}
+		if typ.Size() != table.size {
+			t.Errorf("expected size=%v, got [%v]", table.size, typ.Size())
+		}
+		for i := 0; i < typ.NumField(); i++ {
+			if typ.Field(i).Offset != table.offsets[i] {
+				t.Errorf("expected offset=%v, got [%v]",
+					table.offsets[i],
+					typ.Field(i).Offset)
+			}
+		}
 	}
 }
 
