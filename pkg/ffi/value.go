cbb22596efe4a926af1b989f9175e205add990eb
Sebastien Binet <binet@cern.ch>
2012-09-20 15:48:24 +0200

    properly set fields values of a struct

--- pkg/ffi/value.go
+++ pkg/ffi/value.go
@@ -7,6 +7,34 @@ import (
 	"unsafe"
 )
 
+const ptrSize = unsafe.Sizeof((*byte)(nil))
+
+// TODO: This will have to go away when
+// the new gc goes in.
+func memmove(adst, asrc unsafe.Pointer, n uintptr) {
+	dst := uintptr(adst)
+	src := uintptr(asrc)
+	switch {
+	case src < dst && src+n > dst:
+		// byte copy backward
+		// careful: i is unsigned
+		for i := n; i > 0; {
+			i--
+			*(*byte)(unsafe.Pointer(dst + i)) = *(*byte)(unsafe.Pointer(src + i))
+		}
+	case (n|src|dst)&(ptrSize-1) != 0:
+		// byte copy forward
+		for i := uintptr(0); i < n; i++ {
+			*(*byte)(unsafe.Pointer(dst + i)) = *(*byte)(unsafe.Pointer(src + i))
+		}
+	default:
+		// word copy forward
+		for i := uintptr(0); i < n; i += ptrSize {
+			*(*uintptr)(unsafe.Pointer(dst + i)) = *(*uintptr)(unsafe.Pointer(src + i))
+		}
+	}
+}
+
 // methodName returns the name of the calling method,
 // assumed to be two stack frames above.
 func methodName() string {
@@ -38,40 +66,8 @@ type Value struct {
 	// typ holds the type of the value represented by the Value
 	typ Type
 
-	// val holds the 1-word representation of the value.
-	// If flag's flagIndir bit is set, then val is a pointer to the data.
-	// Otherwise, val is a word holding the actual data.
-	// When the data is smaller than a word, it begins at
-	// the first byte (in the memory address sense) of val.
-	// We use unsafe.Pointer so that the garbage collector
-	// knows that val could be a pointer.
+	// val points at the value of this Value.
 	val unsafe.Pointer
-
-	// flag holds metadata about the value.
-	// The lowest bits are flag bits:
-	//	- flagIndir: val holds a pointer to the data
-	//	- flagAddr: v.CanAddr is true (implies flagIndir)
-	// The next five bits give the Kind of the value.
-	// This repeats typ.Kind() except for method values.
-	// The remaining 23+ bits give a method number for method values.
-	// If flag.kind() != Func, code can assume that flagMethod is unset.
-	// If typ.size > ptrSize, code can assume that flagIndir is set.
-	//flag
-}
-
-type flag uintptr
-
-const (
-	flagRO flag = 1 << iota
-	flagIndir
-	flagAddr
-	flagKindShift      = iota
-	flagKindWidth      = 5 // there are 16 kinds
-	flagKindMask  flag = 1<<flagKindWidth - 1
-)
-
-func (f flag) kind() Kind {
-	return Kind((f >> flagKindShift) & flagKindMask)
 }
 
 // New returns a Value representing a pointer to a new zero value for
@@ -365,10 +361,14 @@ func (v Value) NumField() int {
 	return v.typ.NumField()
 }
 
-func (v *Value) set_field(idx int, f Value) {
-	vv := v.Field(idx)
-	buf := vv.Buffer()
-	copy(buf, f.Buffer())
+func (v *Value) set_field(i int, f Value) {
+
+	// fmt.Printf(":: v=0x%x i=%d f=0x%x...\n", v.UnsafeAddr(), i, f.UnsafeAddr())
+	vv := v.Field(i)
+	memmove(
+		unsafe.Pointer(vv.UnsafeAddr()),
+		unsafe.Pointer(f.UnsafeAddr()),
+		vv.typ.Size())
 }
 
 // SetValue assigns x to the value v.
@@ -468,16 +468,17 @@ func (v Value) SetInt(x int64) {
 		*(*int64)(v.val) = x
 	}
 }
+
 // SetLen sets v's length to n.
 // It panics if v's Kind is not Slice or if n is negative or
 // greater than the capacity of the slice.
 func (v Value) SetLen(n int) {
-        v.mustBe(Slice)
-        s := (*reflect.SliceHeader)(v.val)
-        if n < 0 || n > int(s.Cap) {
-                panic("reflect: slice length out of range in SetLen")
-        }
-        s.Len = n
+	v.mustBe(Slice)
+	s := (*reflect.SliceHeader)(v.val)
+	if n < 0 || n > int(s.Cap) {
+		panic("reflect: slice length out of range in SetLen")
+	}
+	s.Len = n
 }
 
 // SetPointer sets the unsafe.Pointer value v to x.
